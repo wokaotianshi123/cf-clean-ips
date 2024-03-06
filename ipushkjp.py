172b746d967ac04d37ef3d0b9ad5897760acfc21
wokaotianshi123 <56501836+wokaotianshi123@users.noreply.github.com>
2024-03-06 22:01:02 +0800

    Add files via upload

---
+++ ipushkjp.py
@@ -0,0 +1,29 @@
+import re
+import requests
+
+# 指定网址列表
+urls = [
+    "https://raw.githubusercontent.com/Garfyyy/cfip2/main/ip443/JP.txt",
+    "https://raw.githubusercontent.com/Garfyyy/cfip2/main/ip443/HK.txt",
+    "https://raw.githubusercontent.com/Garfyyy/cfip2/main/ip443/US.txt"
+]
+
+# 遍历每个网址
+for url in urls:
+    # 发送HTTP请求获取网页内容
+    response = requests.get(url)
+
+    # 提取网页内容中所有IP地址
+    ip_pattern = r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
+    ip_list = re.findall(ip_pattern, response.text)
+
+    # 根据文件名确定连接符
+    filename = url.split("/")[-1]
+    separator = "#" + filename.split(".")[0]
+
+    # 用连接符连接IP地址,并在每行后添加换行符
+    new_content = "\n".join([ip + separator for ip in ip_list])
+
+    # 将结果写入新文件
+    with open(filename, "w") as f:
+        f.write(new_content + "\n")
