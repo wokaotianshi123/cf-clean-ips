efce2fb7bebe128fa8c1b17d1e0337900d158f67
wokaotianshi123 <56501836+wokaotianshi123@users.noreply.github.com>
2024-03-06 23:11:43 +0800

    Update ipushkjp.py

--- ipushkjp.py
+++ ipushkjp.py
@@ -5,6 +5,8 @@ import requests
 urls = [
     "https://raw.githubusercontent.com/Garfyyy/cfip/main/resip/JP.txt",
     "https://raw.githubusercontent.com/Garfyyy/cfip/main/resip/HK.txt",
+    "https://raw.githubusercontent.com/Garfyyy/cfip/main/resip/IN.txt",
+    "https://raw.githubusercontent.com/Garfyyy/cfip/main/resip/SG.txt",
     "https://raw.githubusercontent.com/Garfyyy/cfip/main/resip/US.txt"
 ]
 
