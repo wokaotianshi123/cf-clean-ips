6028030a0aa31f8d65ec9ab979c831694bccfafb
wokaotianshi123 <56501836+wokaotianshi123@users.noreply.github.com>
2024-03-16 16:37:08 +0800

    Update ipcha.py

--- ipcha.py
+++ ipcha.py
@@ -1,5 +1,4 @@
 import requests
-import os
 
 # 从远程地址读取IP地址
 def read_ips(*remote_urls):
@@ -13,9 +12,8 @@ def read_ips(*remote_urls):
             print(f"An error occurred while fetching IPs from {remote_url}: {e}")
     return ips
 
-
 # 发送批量请求到IP-API.com并处理结果
-def get_geolocation_and_save(ips, file_jg_path, file_jgb_path):
+def get_geolocation_and_save(ips):
     url = "http://ip-api.com/batch"
     payload = [{'query': ip} for ip in ips]
     headers = {
@@ -25,21 +23,21 @@ def get_geolocation_and_save(ips, file_jg_path, file_jgb_path):
         response = requests.post(url, json=payload, headers=headers)
         if response.status_code == 200:
             results = response.json()
-            with open(file_jg_path, 'a') as file_jg, open(file_jgb_path, 'a') as file_jgb:
+            with open('jg.txt', 'a') as file_jg, open('jgb.txt', 'a') as file_jgb:
                 for item in results:
                     if 'query' in item and 'countryCode' in item:
                         # 完整的IP#countrycode记录
-                        output_jg = f"{item['query']}#{item['countryCode']}{os.linesep}"
-                        file_jg.write(output_jg)
+                        output_jg = f"{item['query']}#{item['countryCode']}"
+                        file_jg.write(output_jg + '\n')
                         # 仅IP地址的记录
-                        output_jgb = f"{item['query']}{os.linesep}"
-                        file_jgb.write(output_jgb)
-                        print(output_jg.rstrip())  # 打印完整的IP#countrycode记录
+                        output_jgb = f"{item['query']}"
+                        file_jgb.write(output_jgb + '\n')
+                        print(output_jg)  # 打印完整的IP#countrycode记录
                     elif 'query' in item:
                         # 仅IP地址的记录
-                        output_jgb = f"{item['query']}{os.linesep}"
-                        file_jgb.write(output_jgb)
-                        print(output_jgb.rstrip())  # 打印仅IP地址的记录
+                        output_jgb = f"{item['query']}"
+                        file_jgb.write(output_jgb + '\n')
+                        print(output_jgb)  # 打印仅IP地址的记录
                     else:
                         print(f"Incomplete data for IP: {item['query']}")
         else:
@@ -51,16 +49,13 @@ def get_geolocation_and_save(ips, file_jg_path, file_jgb_path):
 def main():
     # 远程地址列表
     remote_urls = [
-        'https://gitjs.wokaotianshi123.cloudns.org/https://raw.githubusercontent.com/wokaotianshi123/cf-clean-ips/main/ip.txt',
-        'https://gitjs.wokaotianshi123.cloudns.org/https://raw.githubusercontent.com/ymyuuu/IPDB/main/bestproxy.txt'
+        'https://raw.githubusercontent.com/wokaotianshi123/cf-clean-ips/main/ip.txt', 
+        'https://raw.githubusercontent.com/ymyuuu/IPDB/main/bestproxy.txt' 
     ]
     # 读取IP地址
     ips = read_ips(*remote_urls)
     if ips:
-        # 输出文件路径
-        file_jg_path = 'jg.txt'
-        file_jgb_path = 'jgb.txt'
-        get_geolocation_and_save(ips, file_jg_path, file_jgb_path)
+        get_geolocation_and_save(ips)
 
 if __name__ == "__main__":
     main()
