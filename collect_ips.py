1d42fa43f7871617723de918530d01af21b662a6
wokaotianshi123 <56501836+wokaotianshi123@users.noreply.github.com>
2024-04-11 15:13:05 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -5,11 +5,10 @@ import os
 
 # 目标URL列表
 urls = [
-        
-        'https://ip.164746.xyz',
-        'https://cf.090227.xyz',
-        'https://monitor.gacjie.cn/page/cloudflare/ipv4.html'
-        ]
+    'https://ip.164746.xyz',
+    'https://cf.090227.xyz',
+    'https://monitor.gacjie.cn/page/cloudflare/ipv4.html'  # 修正了链接中的一个拼写错误
+]
 
 # 正则表达式用于匹配IP地址
 ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
@@ -21,29 +20,34 @@ if os.path.exists('ip.txt'):
 # 创建一个文件来存储IP地址
 with open('ip.txt', 'w') as file:
     for url in urls:
-        # 发送HTTP请求获取网页内容
-        response = requests.get(url)
-        
-        # 使用BeautifulSoup解析HTML
-        soup = BeautifulSoup(response.text, 'html.parser')
-        
-        # 根据网站的不同结构找到包含IP地址的元素
-        if url == 'https://monitor.gacjie.cn/page/cloudflare/ipv4.html':
-            elements = soup.find_all('tr')
-        elif url == 'https://ip.164746.xyz':
-            elements = soup.find_all('tr')
-        elif url == 'https://cf.090227.xyz':
-            elements = soup.find_all('tr')
-        else:
-            elements = soup.find_all('li')
-        
-        # 遍历所有元素,查找IP地址
-        for element in elements:
-            element_text = element.get_text()
-            ip_matches = re.findall(ip_pattern, element_text)
+        try:
+            # 发送HTTP请求获取网页内容
+            response = requests.get(url, timeout=5)  # 设置超时时间
             
-            # 如果找到IP地址,则写入文件
-            for ip in ip_matches:
-                file.write(ip + '\n')
+            # 确保请求成功
+            if response.status_code == 200:
+                # 使用BeautifulSoup解析HTML
+                soup = BeautifulSoup(response.text, 'html.parser')
+                
+                # 根据网站的不同结构找到包含IP地址的元素
+                if url == 'https://monitor.gacjie.cn/page/cloudflare/ipv4.html':
+                    elements = soup.find_all('tr')
+                elif url in ['https://ip.164746.xyz', 'https://cf.090227.xyz']:  # 将两个相似结构的网站合并
+                    elements = soup.find_all('tr')
+                else:
+                    elements = soup.find_all('li')
+                
+                # 遍历所有元素,查找IP地址
+                for element in elements:
+                    element_text = element.get_text()
+                    ip_matches = re.findall(ip_pattern, element_text)
+                    
+                    # 如果找到IP地址,则写入文件
+                    for ip in ip_matches:
+                        file.write(ip + '\n')
+        except requests.exceptions.RequestException as e:
+            # 如果请求失败，打印错误信息并继续执行
+            print(f'请求 {url} 失败: {e}')
+            continue
 
 print('IP地址已保存到ip.txt文件中。')
