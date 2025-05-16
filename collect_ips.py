f91292ffc8ad94c8be63acbd916865486b296971
wokaotianshi123 <56501836+wokaotianshi123@users.noreply.github.com>
2025-05-16 08:43:04 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -7,6 +7,7 @@ import os
 urls = [
     'https://ip.164746.xyz', 
     'https://cf.090227.xyz', 
+    'https://stock.hostmonit.com/CloudFlareYes',
     'https://www.wetest.vip/page/cloudflare/address_v4.html'  # 修正了链接中的一个拼写错误
 ]
 
